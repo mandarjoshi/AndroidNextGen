8ef6366bc1ff725c4637b01538879fd4f8274eb0
Mandar Joshi <treepiesinc@gmail.com>
2022-03-17 17:44:38 -0400

    added bottom navigation view
    added bottom navigation view

--- app/src/main/java/com/mandarjoshi/androidnextgen/MainActivity.kt
+++ app/src/main/java/com/mandarjoshi/androidnextgen/MainActivity.kt
@@ -2,23 +2,57 @@ package com.mandarjoshi.androidnextgen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //giving toolbar control to nav graph(app_graph.xml)
-        //"label" set for fragment will be set as toolbar title
-        // also back button will be set for child fragments
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()?.let { navController ->
-            val appBarConfiguration = AppBarConfiguration(navController.graph)
-            findViewById<MaterialToolbar>(R.id.toolbar)
-                .setupWithNavController(navController, appBarConfiguration)
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.action_list, R.id.shoppingFragment)
+        )
+
+        findViewById<MaterialToolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
+        setNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+
+    private fun setNavigation(){
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            /*
+            if(destination.id == R.id.full_screen_destination) {
+                toolbar.visibility = View.GONE
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                toolbar.visibility = View.VISIBLE
+                bottomNavigationView.visibility = View.VISIBLE
+            }*/
         }
     }
 }
